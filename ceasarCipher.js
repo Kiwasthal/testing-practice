bde24e4c167335916521fcae14b3bc42b3d77140
Steerpike <imthekoul@gmail.com>
2022-05-12 03:19:45 +0300

    Updated Cipher to work with different cases

--- ceasarCipher.js
+++ ceasarCipher.js
@@ -1,6 +1,14 @@
-export { testNumber };
-let testNumber = (str, shiftNum) =>
-  str
-    .split('')
-    .map(char => String.fromCharCode(char.charCodeAt(0) + shiftNum))
-    .join('');
+export { ceasarCipher };
+let ceasarCipher = (str, shiftNum) =>
+  shiftNum <= 0 || shiftNum > 25
+    ? 'No negative Inputs'
+    : str
+        .split('')
+        .map(char => {
+          if (/^[a-z]+$/i.test(char))
+            return char.charCodeAt(0) + shiftNum >= 123
+              ? String.fromCharCode(char.charCodeAt(0) + shiftNum - 26)
+              : String.fromCharCode(char.charCodeAt(0) + shiftNum);
+          else return char;
+        })
+        .join('');
